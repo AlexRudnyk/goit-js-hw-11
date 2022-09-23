3dbe606766d69c186167276fc3c0eeb984bbd893
AlexRudnyk <alexander.rudnyk@ukr.net>
2022-09-23 21:15:48 +0300

    Добавил готовое задание

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import makePicsMarkup from './makePicsMarkup';
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// import PicsApiService from './pics-api-service';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { Notify } from 'notiflix';
 import PicsApiService from './pics-api-service';
 
@@ -18,9 +16,13 @@ refs.formEl.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
 
 function onSearch(event) {
-  event.preventDefault();
-  picsApiService.query = event.target.elements.searchQuery.value;
-  fetchPics();
+  if (event.target.elements.searchQuery.value !== '') {
+    event.preventDefault();
+    picsApiService.query = event.target.elements.searchQuery.value;
+    picsApiService.resetPage();
+    refs.galleryEl.innerHTML = '';
+    fetchPics();
+  }
 }
 
 async function fetchPics() {
@@ -28,23 +30,36 @@ async function fetchPics() {
     if (picsApiService.query !== '') {
       const photoCard = await picsApiService.fetchPhoto();
       if (photoCard.data.hits.length !== 0) {
-        Notify.success(`Hooray! We found ${photoCard.data.totalHits} images.`);
+        if (picsApiService.page === 1) {
+          Notify.success(
+            `Hooray! We found ${photoCard.data.totalHits} images.`
+          );
+        }
         refs.galleryEl.insertAdjacentHTML(
           'beforeend',
           makePicsMarkup(photoCard.data.hits)
         );
-        picsApiService.incrementPage();
+        refs.loadMoreBtn.classList.remove('load-more');
       } else {
         Notify.failure(
           'Sorry, there are no images matching your search query. Please try again.'
         );
       }
+      picsApiService.incrementPage();
+      refs.loadMoreBtn.disabled = false;
     }
   } catch (error) {
     console.log(error.message);
+    Notify.failure(
+      "We're sorry, but you've reached the end of search results."
+    );
+    refs.loadMoreBtn.classList.add('load-more');
   }
+  var lightbox = new SimpleLightbox('.gallery a');
+  lightbox.refresh();
 }
 
 function onLoadMoreBtnClick() {
+  refs.loadMoreBtn.disabled = true;
   fetchPics();
 }
